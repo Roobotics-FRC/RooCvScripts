b86b58f1cb4329d0ab95e07199a153f69be7196b
Rui-Jie Fang <ruijie.fang@temple.edu>
2017-09-29 18:05:56 -0400

    handle error

--- find_bulb.py
+++ find_bulb.py
@@ -11,15 +11,18 @@ while True:
     # Set up the detector with default parameters.
     detector = cv2.SimpleBlobDetector_create()
     keypoints = detector.detect(im)
-    for keypoint in keypoints:
-        print("d= ",keypoint.pt[0] - X_CENTER," (px), theta= ", keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER * DEGREES_PER_PIXEL, " (degs)")
-        # networktables stuff
-        sd.putNumber('Camera_Pixel_Offset', keypoint.pt[0] - X_CENTER)
-        sd.putNumber('Camera_Angle_Offset', keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER * DEGREES_PER_PIXEL)
-#        sd.putNumber('Received_Gyro_Value', sd.getNumber('Gyro value'))
-        sd.putNumber('Calculated_PID_Setpoint', sd.getNumber('Gyro value') + (keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER*DEGREES_PER_PIXEL))
-    im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    
+    im_with_keypoints = None
+    try:
+        for keypoint in keypoints:
+            print("d= ",keypoint.pt[0] - X_CENTER," (px), theta= ", keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER * DEGREES_PER_PIXEL, " (degs)")
+            # networktables stuff
+            sd.putNumber('Camera_Pixel_Offset', keypoint.pt[0] - X_CENTER)
+            sd.putNumber('Camera_Angle_Offset', keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER * DEGREES_PER_PIXEL)
+            sd.putNumber('Received_Gyro_Value', sd.getNumber('Gyro_Value'))
+            sd.putNumber('Calculated_PID_Setpoint', sd.getNumber('Gyro_Value') + (keypoint.pt[0]*DEGREES_PER_PIXEL - X_CENTER*DEGREES_PER_PIXEL))
+        im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    except KeyError:
+        im_with_keypoints = im
     # Show keypoints
     cv2.imshow("Keypoints", im_with_keypoints)
     if cv2.waitKey(1) & 0xFF == ord('q'):
