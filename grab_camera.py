7e8148e20504b3fdfa8c90ee3f4e8a30642cc8b6
Rui-Jie Fang <ruijie.fang@temple.edu>
2018-01-19 17:13:35 -0500

    Create grab_camera.py

---
+++ grab_camera.py
@@ -0,0 +1,16 @@
+from sys import argv
+
+if len(argv) != 2:
+  print("Usage: " + argv[0] + " <full address to http stream>")
+  exit(1)
+
+
+import cv2
+#cam = cv2.VideoCapture("http://10.43.73.74/mjpg/video.mjpg")
+cam = cv2.VideoCapture(argv[1])
+while True:
+    ret, im = cam.read()
+    cv2.imshow("Camera Stream", im_with_keypoints)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
