19632d018e4a0817362e0643f522747e41df62e0
aaplmath <aaplmath@users.noreply.github.com>
2019-01-19 11:07:31 -0500

    Update to eliminate weird dimensions and support 480p

--- stream_camera.py
+++ stream_camera.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import cv2
 from PIL import Image, ImageTk
 
-width, height = 800, 600
-cap = cv2.VideoCapture('http://10.43.73.74/mjpg/video.mjpg?resolution=320x240')
+width, height = 640, 480
+cap = cv2.VideoCapture('http://10.43.73.74/mjpg/video.mjpg?resolution=640x480')
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -11,13 +11,13 @@ root = tk.Tk()
 root.wm_attributes("-topmost", 1)
 root.bind('<Escape>', lambda e: root.quit())
 
-lmain = tk.Label(root, width=width - 160, height=height - 120)
+lmain = tk.Label(root, width=width, height=height)
 lmain.pack()
 
 def show_frame():
     _, frame = cap.read()
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    cv2image = cv2.resize(cv2image, (0,0), fx=2, fy=2)
+#    cv2image = cv2.resize(cv2image, (0,0), fx=2, fy=2) # if we ever need to resize, this is how
     img = Image.fromarray(cv2image)
     imgtk = ImageTk.PhotoImage(image=img)
     lmain.imgtk = imgtk
